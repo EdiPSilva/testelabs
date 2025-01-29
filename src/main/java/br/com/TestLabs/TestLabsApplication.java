51f05b104638a9d829845ce765a168283ddee82d
edisilva <edipaulinodasilva@gmail.com>
2025-01-29 16:33:41 -0300

    criado estrutura inicial do projeto

---
+++ src/main/java/br/com/TestLabs/TestLabsApplication.java
@@ -0,0 +1,13 @@
+package br.com.TestLabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestLabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestLabsApplication.class, args);
+	}
+
+}
