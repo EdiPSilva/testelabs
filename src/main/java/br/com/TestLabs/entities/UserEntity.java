8aa1894bafc4a5ab4f79bafffdd962b3f044d40c
edisilva <edipaulinodasilva@gmail.com>
2025-01-30 17:39:48 -0300

    renomeando condicao de auto incremento

--- src/main/java/br/com/TestLabs/entities/UserEntity.java
+++ src/main/java/br/com/TestLabs/entities/UserEntity.java
@@ -2,8 +2,6 @@ package br.com.TestLabs.entities;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Builder;
@@ -18,7 +16,6 @@ import java.time.LocalDateTime;
 public class UserEntity {
     @Id
     @Column(name = "id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(name = "name")
     private String name;
